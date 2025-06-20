c128088aaca57441a59321bcc6a4878742c350f0
danrus1100 <danrus228zk@gmail.com>
2025-06-20 22:07:01 +0300

    HotFix

--- src/main/java/com/danrus/durability_visibility_options/mixin/client/DrawContextMixin.java
+++ src/main/java/com/danrus/durability_visibility_options/mixin/client/DrawContextMixin.java
@@ -8,23 +8,20 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
-import net.minecraft.text.Text;
 import net.minecraft.util.math.MathHelper;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+//? if <1.21.6 {
+import net.minecraft.client.render.RenderLayer;
+//?}hot
+
 @Mixin(DrawContext.class)
 public abstract class DrawContextMixin {
-
-    @Shadow public abstract void drawText(TextRenderer par1, Text par2, int par3, int par4, int par5, boolean par6);
 
     //? if >1.21.1 {
     /*@Redirect(
@@ -66,9 +63,9 @@ public abstract class DrawContextMixin {
         //? if <=1.21.1 {
         ClientPlayerEntity clientPlayerEntity = MinecraftClient.getInstance().player;
         //? if >1.20.6 {
-        float tickDelta = MinecraftClient.getInstance().getRenderTickCounter().getTickDelta(true);
-        //?} else
-        /*float tickDelta = MinecraftClient.getInstance().getTickDelta();*/
+        /*float tickDelta = MinecraftClient.getInstance().getRenderTickCounter().getTickDelta(true);
+        *///?} else
+        float tickDelta = MinecraftClient.getInstance().getTickDelta();
         float f = clientPlayerEntity == null ? 0.0F : clientPlayerEntity.getItemCooldownManager().getCooldownProgress(stack.getItem(), tickDelta);
         if (f > 0.0F) {
             int k = y + MathHelper.floor(16.0F * (1.0F - f));
